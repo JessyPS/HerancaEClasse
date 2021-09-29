a565698c85f5ed8dec14f0fd06fc41ca2604b826
JessyPS <89113356+JessyPS@users.noreply.github.com>
2021-09-29 16:22:02 -0300

    Menu de sanduíches, opções de montagem.

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,87 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
-        do{
-            System.out.println("\nMenu Principal\n");
-            System.out.println("1 -> Sanduíches");
-            System.out.println("2 -> Massas");
-            System.out.println("3 -> Bolos");
-            System.out.println("4 -> SAIR");
-            System.out.println("\nDigite a sua opção: ");
+        do {
+            mostrarMenuPrincipal();
+        } while (true);
+    }
 
-            // Configurar Scanner
+    public static void montarSanduiches() {
+        Sanduiche sanduiche = new Sanduiche();
+        String [] ing = new String[10];
+        do {
+            System.out.println("---------------------------");
+            System.out.println("\nMenu de Sanduíches \n");
+            System.out.println("Escolha seus ingredientes ");
+            System.out.println("1 -> Queijo");
+            System.out.println("2 -> Hamburger");
+            System.out.println("3 -> Bacon");
+            System.out.println("4 -> Alface");
+            System.out.println("5 -> Maionese");
+            System.out.println("6 -> Ovo");
+            System.out.println("7 -> Tomate");
+            System.out.println("8 -> Molho especial");
+            System.out.println("9 -> Montar Lanche");
+            System.out.println("0 -> Voltar");
+            System.out.println("Opção: ");
+            //Leitura da opção
+
             Scanner scanner = new Scanner(System.in);
             int opcao = scanner.nextInt();
-
-            //Limpar buffer do teclado (enter dado anteriormente)
-            //scanner.nextInt();
-
-//        System.out.println("Opção digitada: " + opcao);
+            //Tratando a opção
             switch (opcao) {
-                case 1:
-                    System.out.println("Opção escolhida: Sanduíche");
-                    break;
-                case 2:
-                    System.out.println("Opção escolhida: Massas");
-                    break;
-                case 3:
-                    System.out.println("Opção escolhida: Bolos");
-                    break;
-                case 4:
-                    System.out.println("Opção escolhida: Sair do sistema");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Opção inválida! ");
+                case 1 -> sanduiche.adicionarIngrediente("Queijo");
+                case 2 -> sanduiche.adicionarIngrediente("Hamburger ");
+                case 3 -> sanduiche.adicionarIngrediente("Bacon ");
+                case 4 -> sanduiche.adicionarIngrediente("Alface ");
+                case 5 -> sanduiche.adicionarIngrediente("Maionese ");
+                case 6 -> sanduiche.adicionarIngrediente("Ovo ");
+                case 7 -> sanduiche.adicionarIngrediente("Tomate ");
+                case 8 -> sanduiche.adicionarIngrediente("Molho especial");
+                case 9 -> mostrarLanche(sanduiche);
+                default -> System.out.println("Opção inválida! Selecione uma das opções informadas");
             }
-        }while(true);
+        } while (true);
+    }
 
+    public static void mostrarLanche(Sanduiche sanduiche) {
+        System.out.println("Ingredientes adicionados ao seu sanduíche: ");
+        System.out.println(sanduiche.toString());
+    }
+
+    public static void mostrarMenuMassas() {}
+
+    public static void mostrarMenuBolos() {}
+
+    private static void mostrarMenuPrincipal() {
+        System.out.println("\nMenu Principal\n");
+        System.out.println("1 -> Sanduíches");
+        System.out.println("2 -> Massas");
+        System.out.println("3 -> Bolos");
+        System.out.println("0 -> SAIR");
+        System.out.println("\nDigite a sua opção: ");
+
+        // Configurar Scanner
+        Scanner scanner = new Scanner(System.in);
+        int opcao = scanner.nextInt();
+
+        System.out.println("Opção digitada: " + opcao);
+        String msg = "Opção escolhida: ";
+        switch (opcao) {
+            case 1 -> {
+                System.out.println(msg + "Sanduíche");
+                montarSanduiches();
+            }
+            case 2 -> System.out.println(msg + "Massas");
+            case 3 -> System.out.println(msg + "Bolos");
+            case 4 -> {
+                System.out.println(msg + "Sair do sistema");
+                System.exit(0);
+            }
+            default -> System.out.println("Opção inválida! Selecione outra opção. ");
+        }
     }
 }
+
--- src/Sanduiche.java
+++ src/Sanduiche.java
@@ -1,18 +1,44 @@
-public class Sanduiche extends Lanche{
+import java.util.Arrays;
 
-    private String [] ingredientes = new String[10];
+public class Sanduiche extends Lanche {
 
-    public Sanduiche () {
+    //Controlar a quantidade de ingredientes no array
+    private int contIngredientes = 0;
+
+    private String[] ingredientes = new String[10];
+
+    public Sanduiche() {
         super.setTempoDePreparo(15);
     }
 
     //Get e set da subclasse Sanduiche
-
     public String[] getIngredientes() {
         return ingredientes;
     }
 
     public void setIngredientes(String[] ingredientes) {
         this.ingredientes = ingredientes;
+    }
+
+    public void adicionarIngrediente(String ingrediente) {
+        if (contIngredientes < ingredientes.length) {
+            this.ingredientes[contIngredientes] = ingrediente;
+            contIngredientes++;
+        } else {
+            System.out.println("Número máximo de ingredientes atingido! ");
+        }
+    }
+
+    @Override
+    public String toString() {
+        String lanche = "( ";
+        for (int i =0; i <ingredientes.length; i++) {
+            if(ingredientes [i] != null) {
+                lanche += ingredientes[i];
+                lanche += ", ";
+            }
+        }
+        lanche += " )";
+        return lanche;
     }
 }
